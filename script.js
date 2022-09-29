3c48baf6aea242200acf320ebc8b20f00a4b9c36
Death-thekidd <ohiemidivine7@gmail.com>
2022-09-29 16:19:12 +0100

    update--sticky-navigator

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const sectionHeaderEl = document.querySelector(".fake-div");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    console.log(entries);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeaderEl);
